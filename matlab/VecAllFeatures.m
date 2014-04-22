7154e62e4313b691720335db58fdb2104f789e9f
Jean-Alix <jean-alix.david@laposte.net>
2014-04-22 17:02:01 +0200

    functions 10 11 12

---
+++ matlab/VecAllFeatures.m
@@ -0,0 +1,7 @@
+function fmat = VecAllFeatures(all_ftypes, W, H)
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ matlab/VecFeature.m
@@ -0,0 +1,7 @@
+function ftype_vec = VecFeature(ftype, W, H)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
