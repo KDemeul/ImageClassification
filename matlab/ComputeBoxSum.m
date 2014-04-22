5049bcdf38d2e9f0e2b9f1cb7cdbc12a314fa010
KDemeul <kilian.demeule@gmail.com>
2014-04-22 16:31:11 +0200

    Fixed strange bug

--- matlab/ComputeBoxSum.m
+++ matlab/ComputeBoxSum.m
@@ -2,7 +2,7 @@ function A = ComputeBoxSum(ii_im, x, y, w, h)
 %ComputeBoxSum computes the sum of the pixel values within a rectanglar area using the integral image.
 A = 0;
 if(x==1 && y==1)
-        A = ii_im(y+h-1,x+w-1);
+    A = ii_im(y+h-1,x+w-1);
 else
     if(x==1)
         A = ii_im(y+h-1,x+w-1)-ii_im(y-1,x+w-1);
