e632ae10ccadf40dce8fb66d7b80e4dee47ad1f7
Jean-Alix <jean-alix.david@laposte.net>
2014-04-22 14:49:39 +0200

    function1

---
+++ matlab/LoadIm.m
@@ -0,0 +1,22 @@
+function [im, ii_im] = LoadIm(im_fname)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+im = imread(im_fname);
+im = double(im);
+
+im_vec = im(:);
+
+mu = mean(im_vec);
+sigma = std(im_vec);
+
+if sigma == 0
+    im = (im-mu);
+else
+    im = (im-mu)./sigma;
+end
+
+ii_im = cumsum(cumsum(im,2),1);
+
+end
+
