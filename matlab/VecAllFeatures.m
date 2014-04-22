c6b26b4b4967f42c06f16449ead493aad76ae568
Jean-Alix <jean-alix.david@laposte.net>
2014-04-22 17:12:54 +0200

    VecAllFeatures

--- matlab/VecAllFeatures.m
+++ matlab/VecAllFeatures.m
@@ -2,6 +2,12 @@ function fmat = VecAllFeatures(all_ftypes, W, H)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
+all_size = size(all_ftypes);
+fmat = zeros(W*H,all_size);
+
+for i=1:all_size
+    fmat(:,i) = VecFeature(all_ftypes(i),W,H);
+end
 
 end
 
