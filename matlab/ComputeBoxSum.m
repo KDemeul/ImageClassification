88abf05018180d8ba90bdb403521f6a9d7564ec8
KDemeul <kilian.demeule@gmail.com>
2014-04-22 14:24:53 +0200

    Added ComputeBoxSum

---
+++ matlab/ComputeBoxSum.m
@@ -0,0 +1,8 @@
+function A = ComputeBoxSum(ii_im, x, y, w, h)
+%ComputeBoxSum computes the sum of the pixel values within a rectangular area using the integral image.
+
+new_ii = ii_im(y:y+h-1,x:x+w-1)
+A = sum(sum(new_ii,2),1);
+
+end
+
