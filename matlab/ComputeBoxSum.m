d463bc34209ea295959175cddf44e00bc8838308
KDemeul <kilian.demeule@gmail.com>
2014-04-22 16:20:13 +0200

    Fixed bound problem

--- matlab/ComputeBoxSum.m
+++ matlab/ComputeBoxSum.m
@@ -1,6 +1,19 @@
 function A = ComputeBoxSum(ii_im, x, y, w, h)
 %ComputeBoxSum computes the sum of the pixel values within a rectanglar area using the integral image.
-A = ii_im(y+h-1,x+w-1)+ii_im(y-1,x-1)-ii_im(y-1,x+w-1)-ii_im(y+h-1,x-1);
+A = 0;
+if(x==1 && y==1)
+        A = ii_im(y+h-1,x+w-1);
+else
+    if(x==1)
+        A = ii_im(y+h-1,x+w-1)-ii_im(y-1,x+w-1);
+    else
+        if(y==1)
+            A = ii_im(y+h-1,x+w-1)-ii_im(y+h-1,x-1);
+        else
+            A = ii_im(y+h-1,x+w-1)+ii_im(y-1,x-1)-ii_im(y-1,x+w-1)-ii_im(y+h-1,x-1);
+        end
+    end
+end
 
 end
 
