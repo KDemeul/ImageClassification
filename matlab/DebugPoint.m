76e7168bd4a9fd3b2516440d0f8364406bee2f29
KDemeul <kilian.demeule@gmail.com>
2014-04-22 15:50:09 +0200

    Fully functionnal functions 1->6

--- matlab/DebugPoint.m
+++ matlab/DebugPoint.m
@@ -10,7 +10,7 @@ close all
 %% DEBUG POINTS
 
 % DEBUG POINT 1
-[ii,ii_im] = LoadIm('TrainingImages/FACES/face00001.bmp');
+[im,ii_im] = LoadIm('TrainingImages/FACES/face00001.bmp');
 
 dinfo1 = load('DebugInfo/debuginfo1.mat');
 eps = 1e-6;
@@ -18,15 +18,18 @@ s1 = sum(abs(dinfo1.im(:)-im(:)) > eps);
 s2 = sum(abs(dinfo1.ii_im(:)-ii_im(:)) > eps);
 
 if (s1 == 0 && s2 ==0)
-    disp('Debug point 1 correct')
+    disp('Debug point 1 correct');
 end
 
 % DEBUG POINT 2
 
 dinfo2 = load('DebugInfo/debuginfo2.mat');
 x = dinfo2.x; y = dinfo2.y; w = dinfo2.w; h = dinfo2.h;
-abs(dinfo2.f1 - FeatureTypeI(ii_im, x, y, w, h)) > eps
-abs(dinfo2.f2 - FeatureTypeII(ii_im, x, y, w, h)) > eps
-abs(dinfo2.f3 - FeatureTypeIII(ii_im, x, y, w, h)) > eps
-abs(dinfo2.f4 - FeatureTypeIV(ii_im, x, y, w, h)) > eps
+s1 = abs(dinfo2.f1 - FeatureTypeI(ii_im, x, y, w, h)) > eps;
+s2 = abs(dinfo2.f2 - FeatureTypeII(ii_im, x, y, w, h)) > eps;
+s3 = abs(dinfo2.f3 - FeatureTypeIII(ii_im, x, y, w, h)) > eps;
+s4 = abs(dinfo2.f4 - FeatureTypeIV(ii_im, x, y, w, h)) > eps;
 
+if(s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0)
+    disp('Debug point 2 correct');
+end
