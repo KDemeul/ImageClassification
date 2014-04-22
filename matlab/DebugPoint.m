95f0e6b901673681614a6d773a6ecb47c6fdce7a
KDemeul <kilian.demeule@gmail.com>
2014-04-22 18:16:51 +0200

    Final commit for part I

--- matlab/DebugPoint.m
+++ matlab/DebugPoint.m
@@ -10,47 +10,61 @@ close all
 
 %% DEBUG POINTS
 
-% DEBUG POINT 1
-[im,ii_im] = LoadIm('TrainingImages/FACES/face00001.bmp');
+% % DEBUG POINT 1
+% [im,ii_im] = LoadIm('TrainingImages/FACES/face00001.bmp');
+% 
+% dinfo1 = load('DebugInfo/debuginfo1.mat');
+% eps = 1e-6;
+% s1 = sum(abs(dinfo1.im(:)-im(:)) > eps);
+% s2 = sum(abs(dinfo1.ii_im(:)-ii_im(:)) > eps);
+% 
+% if (s1 == 0 && s2 ==0)
+%     disp('Debug point 1 correct');
+% end
+% 
+% % DEBUG POINT 2
+% 
+% dinfo2 = load('DebugInfo/debuginfo2.mat');
+% x = dinfo2.x; y = dinfo2.y; w = dinfo2.w; h = dinfo2.h;
+% s1 = abs(dinfo2.f1 - FeatureTypeI(ii_im, x, y, w, h)) > eps;
+% s2 = abs(dinfo2.f2 - FeatureTypeII(ii_im, x, y, w, h)) > eps;
+% s3 = abs(dinfo2.f3 - FeatureTypeIII(ii_im, x, y, w, h)) > eps;
+% s4 = abs(dinfo2.f4 - FeatureTypeIV(ii_im, x, y, w, h)) > eps;
+% 
+% if(s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0)
+%     disp('Debug point 2 correct');
+% end
+% 
+% % DEBUG POINT 4
+% 
+% dirname = 'TrainingImages/FACES/';
+% dinfo4 = load('DebugInfo/debuginfo4.mat');
+% ni = dinfo4.ni;
+% all_ftypes = dinfo4.all_ftypes;
+% im_sfn = 'FaceData.mat';
+% f_sfn = 'FeaturesToMat.mat';
+% rng(dinfo4.jseed);
+% LoadSaveImData(dirname,ni,im_sfn);
+% ComputeSaveFData(all_ftypes,f_sfn);
+% load(im_sfn);
+% load(f_sfn);
+% s1 = max(max(abs(dinfo4.fmat - fmat))) > eps;
+% s2 = max(max(abs(dinfo4.ii_ims - ii_ims))) > eps;
+% 
+% if(s1==0 && s2==0)
+%     disp('Debug point 4 correct')
+% end
 
-dinfo1 = load('DebugInfo/debuginfo1.mat');
-eps = 1e-6;
-s1 = sum(abs(dinfo1.im(:)-im(:)) > eps);
-s2 = sum(abs(dinfo1.ii_im(:)-ii_im(:)) > eps);
+%% CARREFUL VERY LONG
+% % DEBUG POINT 5
+% 
+% dinfo5 = load('DebugInfo/debuginfo5.mat');
+% np = dinfo5.np;
+% nn = dinfo5.nn;
+% all_ftypes = dinfo5.all_ftypes;
+% rng(dinfo5.jseed);
+% GetTrainingData(all_ftypes, np, nn);
 
-if (s1 == 0 && s2 ==0)
-    disp('Debug point 1 correct');
-end
-
-% DEBUG POINT 2
-
-dinfo2 = load('DebugInfo/debuginfo2.mat');
-x = dinfo2.x; y = dinfo2.y; w = dinfo2.w; h = dinfo2.h;
-s1 = abs(dinfo2.f1 - FeatureTypeI(ii_im, x, y, w, h)) > eps;
-s2 = abs(dinfo2.f2 - FeatureTypeII(ii_im, x, y, w, h)) > eps;
-s3 = abs(dinfo2.f3 - FeatureTypeIII(ii_im, x, y, w, h)) > eps;
-s4 = abs(dinfo2.f4 - FeatureTypeIV(ii_im, x, y, w, h)) > eps;
-
-if(s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0)
-    disp('Debug point 2 correct');
-end
-
-% DEBUG POINT 3
-
-dirname = 'TrainingImages/FACES/';
-dinfo4 = load('DebugInfo/debuginfo4.mat');
-ni = dinfo4.ni;
-all_ftypes = dinfo4.all_ftypes;
-im_sfn = 'FaceData.mat';
-f_sfn = 'FeaturesToMat.mat';
-rng(dinfo4.jseed);
-LoadSaveImData(dirname,ni,im_sfn);
-ComputeSaveFData(all_ftypes,f_sfn);
-load(im_sfn);
-load(f_sfn);
-s1 = max(max(abs(dinfo4.fmat - fmat))) > eps;
-s2 = max(max(abs(dinfo4.ii_ims - ii_ims))) > eps;
-
-if(s1==0 && s2==0)
-    disp('Debug point 4 correct')
-end
+Fdata = load('FaceData.mat');
+NFData = load('NonFaceData.mat');
+FTData = load('FeaturesToUse.mat');
--- matlab/GetTrainingData.m
+++ matlab/GetTrainingData.m
@@ -2,8 +2,8 @@ function GetTrainingData(all_ftypes, np, nn)
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
 
-LoadSaveImData('TrainingImages\FACES',np,'FaceData.mat');
-LoadSaveImData('TrainingImages\NFACES',nn,'NonFaceData.mat');
+LoadSaveImData('TrainingImages/FACES',np,'FaceData.mat');
+LoadSaveImData('TrainingImages/NFACES',nn,'NonFaceData.mat');
 ComputeSaveFData(all_ftypes,'FeaturesToUse.mat');
 
 end
