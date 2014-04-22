733e7b96b0e83280155ad539ac2f8f1a553a88a0
Jean-Alix <jean-alix.david@laposte.net>
2014-04-22 17:05:53 +0200

    VecFeature

--- matlab/VecFeature.m
+++ matlab/VecFeature.m
@@ -2,6 +2,12 @@ function ftype_vec = VecFeature(ftype, W, H)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
+x = ftype(2);
+y = ftype(3);
+w = ftype(4);
+h = ftype(5);
+
+ftype_vec = VecBoxSum(x,y,w,h,W,H) - VecBoxSum(x,y+h,w,h,W,H);
 
 end
 
