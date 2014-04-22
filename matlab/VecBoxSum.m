e135e8054e481c7ad1c12f4be47187c98d8c7cad
KDemeul <kilian.demeule@gmail.com>
2014-04-22 17:03:03 +0200

    Added function VecBoxSum

---
+++ matlab/VecBoxSum.m
@@ -0,0 +1,30 @@
+function b_vec = VecBoxSum(x, y, w, h, W, H)
+%VecBoxSum ii_im(:)’*b_vec equals ComputeBoxSum(ii_im, x, y, w, h)
+% With ComputeBoxSum(ii_im, x, y, w, h) =
+%      ii_im(y+h-1,x+w-1)+ii_im(y-1,x-1)-ii_im(y-1,x+w-1)-ii_im(y+h-1,x-1);
+
+% Memo: If M_flat = M(:) then M(i,j) = M_flat((j-1)*nb_col+i)
+
+b=zeros(H,W);
+
+if(x==1 && y==1)
+    b(y+h-1,x+w-1) = 1;
+else
+    if(x==1)
+        b(y+h-1,x+w-1) = 1;
+        b(y-1,x+w-1) = -1;
+    else
+        if(y==1)
+            b(y+h-1,x+w-1)=1;
+            b(y+h-1,x-1)=-1;
+        else
+            b(y+h-1,x+w-1)=1;
+            b(y-1,x-1)=1;
+            b(y-1,x+w-1)=-1;
+            b(y+h-1,x-1)=-1;
+        end
+    end
+end
+
+b_vec = b(:);
+end
