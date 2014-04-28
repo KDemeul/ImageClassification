a28a8724057258baa4d4a87a3eb04d94e9b5b505
Jean-Alix <jean-alix.david@laposte.net>
2014-04-28 16:36:34 +0200

    LearnWeakClassifier

---
+++ matlab/LearnWeakClassifier.m
@@ -0,0 +1,22 @@
+function [theta, p, err] = LearnWeakClassifier(ws, fs, ys)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+mup = sum(ws.*fs.*ys)/sum(ws.*ys);
+mun = sum(ws.*fs.*(1-ys))/sum(ws.*(1-ys));
+
+theta = (mup+mun)/2;
+
+epsilon_moins_un = sum(ws.*abs(ys-double(-fs<-theta)));
+epsilon_un = sum(ws.*abs(ys-double(fs<theta)));
+
+[~,indice_p] = min([epsilon_moins_un,epsilon_un]);
+if indice_p==1
+    p = -1;
+else
+    p = 1;
+end
+
+err = sum(ws.*abs(ys-double(p*fs<p*theta)));
+
+end
