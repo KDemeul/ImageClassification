811b7664122734b853dc82d40c81dc60546f3bb9
Jean-Alix <jean-alix.david@laposte.net>
2014-04-30 14:58:44 +0200

    ApplyDetector

--- matlab/MakeClassifierPic.m
+++ matlab/MakeClassifierPic.m
@@ -4,10 +4,10 @@ function cpic = MakeClassifierPic(all_ftypes, chosen_f, alphas, ps, W, H)
 
 cpic = zeros(H,W);
 fpic = zeros(H,W,size(chosen_f,1));
-for i=1:size(chosen_f,2)
+for i=1:size(chosen_f,1)
     fpic(:,:,i) = MakeFeaturePic(all_ftypes(chosen_f(i),:),W,H);
 end
-for i=1:size(alphas,2)
+for i=1:size(alphas,1)
     cpic = cpic + alphas(i)*ps(i)*fpic(:,:,i);
 end
 
