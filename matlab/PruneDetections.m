75f4be425248858e1dbce8aa6915e40f5cc08bb2
Jérémy Charles Stéphane Pouech <pouech@csc.kth.se>
2014-04-30 18:27:55 +0200

    PruneDetections update

--- matlab/PruneDetections.m
+++ matlab/PruneDetections.m
@@ -1,14 +1,14 @@
 function fdets = PruneDetections(dets)
 nd = size(dets,1);
-L = dets(1,3);
 
 % FIND OVERLAPPING
 D = zeros(nd);
-rho = 50;
+rho = 0.5;
 for i = 1:nd
     for j = i+1:nd
-        int = rectint(dets(1,:),dets(5,:));
-        if(int > rho)
+        int = rectint(dets(i,:),dets(j,:));
+        uni = dets(i,3)^2 + dets(j,3)^2 - int;
+        if(int/uni > rho)
             D(i,j) = 1;
             D(j,i) = 1;
         end
@@ -19,16 +19,13 @@ end
 [S,C] = graphconncomp(sparse(D));
 fdets = zeros(S,4);
 for s = 1:S
-    xnew = 0;
-    ynew = 0;
+    detnew = [0,0,0,0];
     Set_ind_rect = find(C == s);
     for ind = Set_ind_rect
-        xnew = xnew + dets(ind,1);
-        ynew = ynew + dets(ind,2);
+        detnew = detnew + dets(ind,:);
     end
-    xnew = xnew/size(Set_ind_rect,2);
-    ynew = ynew/size(Set_ind_rect,2);
-    fdets(s,:) = [xnew,ynew,L,L];
+    detnew = detnew/size(Set_ind_rect,2);
+    fdets(s,:) = detnew;
 end
 end
 
